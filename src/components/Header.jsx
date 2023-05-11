1b1a96326c921cc1efae0cd7b6a7073efbf43ff7
Pavel <noonno20445@yandex.ru>
2023-05-12 01:32:32 +0300

    styled header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-
+import './styles.css'
 
 function Header(props) {
 
     const {setMode = Function.prototype} = props;
 
     return  <nav className='#8e24aa purple darken-1'>
-                <div className="nav-wrapper #8e24aa purple darken-1">
-                    <a href="#" className="brand-logo">shop</a>
-                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <div className="nav-wrapper">
+                    <a href="#" className="logo">shop</a>
+                    
+                    <ul id="nav-mobile" className="right">
+                        <li className="separator"></li>
                         <li onClick={() => setMode('fortnite')}><a href="sass.html">fortnite</a></li>
                         <li onClick={() => setMode('csgo')}><a href="#">csgo</a></li>
                         <li><a href="https://github.com/PaveqP/React_shop">info</a></li>
